1e09ccb7261b8072254f1d9559941c671e8a8574
atharva <voidz7447@gmail.com>
2024-06-23 00:53:13 +0530

    23 june 2024 1:00 AM

--- README.md
+++
@@ -1,6 +0,0 @@
-<h1 align="center">
-    <a href="https://github.com/voidz7/dotfiles">
-    </a>
-    <br>
-</h1>
-
--- init.lua
+++ init.lua
@@ -1,65 +1,73 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "--branch=stable",
     lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
 
+require("lazy").setup({
+    {
+    'echasnovski/mini.nvim',
+    config = function()
+      require('mini.ai').setup { n_lines = 500 }
+      require('mini.surround').setup()
+      local statusline = require 'mini.statusline'
+      statusline.setup { use_icons = true }
+      statusline.section_location = function()
+        return '%2l:%-2v'
+      end
+    end,
+  },
+  'neovim/nvim-lspconfig',
+  'nvim-telescope/telescope.nvim',
+  'nvim-lua/plenary.nvim'
+
+})
 
 
 vim.g.mapleader = " "
+vim.opt.number = false
+vim.opt.termguicolors = false
+vim.opt.mouse = "a"
+vim.opt.showmode = false
+vim.opt.wrap = false
+vim.opt.breakindent = true
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+-- vim.opt.signcolumn = "yes"
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.opt.inccommand = "split"
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+vim.opt.hlsearch = true
+vim.cmd 'colorscheme molokai'
 
-require("lazy").setup({
-    "nvim-lua/plenary.nvim",
-    "neovim/nvim-lspconfig",
-    "nvim-telescope/telescope.nvim",
-    "echasnovski/mini.nvim",
-    "nvim-treesitter/nvim-treesitter",
-    "sainnhe/everforest"
 
-},{})
-
-
-
-require('mini.basics').setup()
-require('mini.files').setup()
-require('mini.comment').setup()
-require('mini.completion').setup()
-require('mini.tabline').setup()
-require('mini.pick').setup()
-require('mini.starter').setup()
-require('mini.trailspace').setup()
-
-
-
----- mappings
 local builtin = require('telescope.builtin')
-local opts = { noremap=true, silent=true }
--- telescope
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 vim.keymap.set('n', '<space>ff', builtin.find_files, {})
 vim.keymap.set('n', '<space>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<space>fb', builtin.buffers, {})
 vim.keymap.set('n', '<space>fh', builtin.help_tags, {})
--- LSP
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-vim.keymap.set('n', '<space>m', "Go<esc>:put =strftime('%Y-%m-%d')<cr>o============<cr>", opts)
 
--- map the following keys only after the lsp attached hehe
 local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
+  vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+  vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
+  vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
   local bufopts = { noremap=true, silent=true, buffer=bufnr }
   vim.keymap.set('n', 'gd', builtin.lsp_definitions, bufopts)
   vim.keymap.set('n', 'gi', builtin.lsp_implementations, bufopts)
@@ -70,11 +78,25 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
 end
 
--- require('lspconfig')['clangd'].setup{
--- 	on_attach = on_attach,
--- }
+vim.api.nvim_create_autocmd("textyankpost", {
+	desc = "highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
 
-vim.opt.tabstop=4
-vim.opt.softtabstop=4
-vim.opt.shiftwidth=4
-vim.opt.scrolloff = 10
+-- lsp 
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = '*.go',
+  callback = function()
+    vim.lsp.buf.format()
+    vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
+  end
+})
+
+require('lspconfig')['gopls'].setup{
+    on_attach = on_attach,
+}
+
+-- vim: ts=2 sts=2 sw=2 et
